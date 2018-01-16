7e042c1d77ee884cd54e4654d07cd6f23d4ded8c
waqas <waqasnasir818@gmail.com>
2018-01-16 23:39:59 +0500

    deployed to test netwrok

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+	 rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x95730b557Ea16aBA4A6b11F814eb3A2A544d682F", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
